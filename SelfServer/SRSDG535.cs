6cb7c43d4feaf0173e73aa541d72cc9f46092488
Xiangdong <zhangxd6@hotmail.com>
2018-07-29 15:47:36 -0500

    SRS

---
+++ SelfServer/SRSDG535.cs
@@ -0,0 +1,55 @@
+﻿using NationalInstruments.NI4882;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfServer
+{
+    public class SRSDG535
+    {
+        private static SRSDG535 _instance = new SRSDG535();
+        protected Device device;
+        private Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static SRSDG535 Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        private SRSDG535()
+        {
+            device = new Device(0, new Address(15, 96));
+            device.Write("DS init");
+            logger.Trace(String.Format("the SRSDG535 infomation:{0}", ""));
+            device.Write("DS");
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="target">A is 2, channel B is 3 and trigger is 1</param>
+        /// <param name="delay"></param>
+        /// <param name="source"></param>
+        public void SetDelay(int target, double delay, int source=1)
+        {
+            device.Write($"DT {target},{source},{delay}");
+
+        }
+    }
+}
--- SelfServer/StepScope.cs
+++ SelfServer/StepScope.cs
@@ -8,44 +8,57 @@ using System.Threading.Tasks;
 
 namespace SelfServer
 {
-  public class StepScope : ScopeHubBase
-  {
-    protected Device voltageDevice;
-    public void Start(double startV, double endV, double stepV, int numberCurve=10)
+    public class StepScope : ScopeHubBase
     {
-      base.Start();
-      voltageDevice = new Device(0, new Address(2, 96));
-      voltageDevice.Write("*IDN?");
-      string data = voltageDevice.ReadString();
-
-      voltageDevice.Write(":INST:NSEL 1");
-      voltageDevice.Write(":OUTP ON");
-      voltageDevice.Write(":SOUR:VOLT 1.1");
-      voltageDevice.Write(":INST:NSEL?");
-
-      Task.Factory.StartNew(() =>
-      {
-        ConfigureLogger();
-        InitScope();
-        for(double v = startV; v < endV; v = v + stepV)
+        private SyncAlliedCamera cameraCtl = SyncAlliedCamera.Instance;
+        private SRSDG535 sRSDG535 = SRSDG535.Instance;
+        protected Device voltageDevice;
+        public void Start(double startV, double endV, double stepV, int numberCurve = 10)
         {
-          if (ct.IsCancellationRequested)
-          {
-            return;
-          }
-          var instruction = $":SOUR:VOLT {v}";
-          voltageDevice.Write(instruction);
-          this.curveNumber = 0;
-          this.sumDData = new List<Server.CurvePoint>();
-          raw.Trace($"{v} V");
-          translated.Debug($"{v} V");
-          for(int i=0;i< numberCurve; i++)
-          {
-            this.GetScopeCurve();
-          }
-          this.AggreateCurve();
+            base.Start();
+            cameraCtl.Start();
+
+            voltageDevice = new Device(0, new Address(2, 96));
+            voltageDevice.Write("*IDN?");
+            string data = voltageDevice.ReadString();
+
+            voltageDevice.Write(":INST:NSEL 1");
+            voltageDevice.Write(":OUTP ON");
+            voltageDevice.Write(":SOUR:VOLT 1.1");
+            voltageDevice.Write(":INST:NSEL?");
+
+            Task.Factory.StartNew(() =>
+            {
+                ConfigureLogger();
+                InitScope();
+                for (double v = startV; v < endV; v = v + stepV)
+                {
+                    for (double d = 0.0; d < 1.0e-5; d = d + 1.0e-6)
+                    {
+                        if (ct.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                        sRSDG535.SetDelay(2, d);
+
+                        var instruction = $":SOUR:VOLT {v}";
+                        voltageDevice.Write(instruction);
+                        this.curveNumber = 0;
+                        this.sumDData = new List<Server.CurvePoint>();
+                        raw.Trace($"{v} V");
+                        translated.Debug($"{v} V");
+                        for (int i = 0; i < numberCurve; i++)
+                        {
+                            this.GetScopeCurve();
+                        }
+                        this.AggreateCurve();
+                        cameraCtl.AccquireImage($"data/delay_{d}");
+                    }
+                }
+                cameraCtl.StopCamera();
+
+
+            }, ct);
         }
-      }, ct);
     }
-  }
 }
