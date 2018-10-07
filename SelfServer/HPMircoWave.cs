0f30e442e48fabb56db98dde4e1b0c14f7ea4b4a
Xiangdong <zhangxd6@hotmail.com>
2018-10-07 16:11:59 -0500

    microvwave

---
+++ SelfServer/HPMircoWave.cs
@@ -0,0 +1,41 @@
+﻿using NationalInstruments.NI4882;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfServer
+{
+    public class HPMircoWave
+    {
+        private static HPMircoWave instnace = new HPMircoWave();
+
+        public static HPMircoWave Instance
+        {
+            get
+            {
+                return instnace;
+            }
+        }
+
+        private Device device;
+
+        private HPMircoWave()
+        {
+            device = new Device(0, new Address(19, 96));
+            //Set it as remote
+            device.Write("REM");
+
+        }
+        /// <summary>
+        /// Q345678Z0 => 3.45678 GZ
+        /// 
+        /// </summary>
+        /// <param name="freq"></param>
+        public void SetFrequnecy(string freq)
+        {
+            device.Write(freq);
+        }
+    }
+}
